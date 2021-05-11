6263dfa130b0f4e3c05bc2e1d25a4fb738021f16
linyue <2548360113@qq.com>
2021-05-11 21:55:43 +0800

    小作业

---
+++ 5.11/src/palindrome.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class palindrome {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        //nextline方法可以读取空字符串
+        String str = sc.nextLine();
+        String str2 = new StringBuilder(str).reverse().toString();
+        if (str.equals(str2)){
+            System.out.println("是回文");
+        }else{
+            System.out.println("不是回文");
+        }
+    }
+}
+
---
+++ 5.11/src/rollback.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class rollback {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String str = sc.nextLine();
+        String str2 = new StringBuilder(str).reverse().toString();
+        System.out.println(str);
+        System.out.println(str2);
+    }
+}
